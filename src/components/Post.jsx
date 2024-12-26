2712320c167aaec7901fe1567f418f5518faafbb
Vitor Matheus de Moraes <vitor.matheusm@gmail.com>
2024-12-26 19:28:26 -0300

    fix: correção dos erros de key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -49,9 +49,9 @@ function handleNewCommentChange () {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph'){
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -71,7 +71,7 @@ function handleNewCommentChange () {
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content= {comment}/>
+                    return <Comment key={comment} content= {comment}/>
                 })}
             </div>
         </article>
