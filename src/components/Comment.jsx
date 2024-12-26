a16a00307a53d10e9e230a298d627e72eacb0585
Vitor Matheus de Moraes <vitor.matheusm@gmail.com>
2024-12-26 17:18:19 -0300

    feat: realizando a inserção de comntários reais

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment () {
+export function Comment ({content}) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/121506301?v=4" alt="" />
@@ -19,7 +19,7 @@ export function Comment () {
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,48 +1,78 @@
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
-import styles from './Post.module.css'
+import {format, formatDistanceToNow} from 'date-fns';
+import {ptBR } from 'date-fns/locale/pt-BR';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+import { useState } from 'react';
 
-export function Post (props) {
-    console.log(props)
+export function Post ({author, publishedAt, content}) {
+    const [comments, setComments] = useState([
+        'Muito foda tio, ce é louco!'
+    ])
+    const [newCommentText, setNewCommentText] = useState ('')
+
+    const publishedDateFormatted = format (publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+ const publisedDateRelativeToNow = formatDistanceToNow (publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+ })
+
+ function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+ }
+
+function handleNewCommentChange () {
+    setNewCommentText(event.target.value)
+}
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/121506301?v=4" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Vitor Mores</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="22 de Dezembro às 22:22h" dateTime="2024-12-22 22:22:00">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>                  
+                    {publisedDateRelativeToNow }
+                </time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p><a href="">jane.design/doctorcare</a></p>
-
-                <p><a href="">#novoprojeto </a>
-                    <a href="">#nlw  </a>
-                    <a href="">#rocketseat</a></p>
+                {content.map(line => {
+                    if (line.type == 'paragraph'){
+                        return <p>{line.content}</p>;
+                    } else if (line.type == 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                name="comment"
                 placeholder='Escreva um comentário'
+                value={newCommentText}
+                onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type='submit'>Publicar</button>
                 </footer>    
             </form>
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment content= {comment}/>
+                })}
             </div>
         </article>
     )
